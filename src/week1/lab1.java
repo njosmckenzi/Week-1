9bfb038e4986a15a66115e43ad471e3d47c1b661
McKenzi Njos <njosmckenzi@gmail.com>
2022-07-13 19:00:28 -0500

    first commit

---
+++ src/week1/lab1.java
@@ -0,0 +1,29 @@
+package week1;
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		
+		int availablePlaneSeats = 5;
+		
+		double costOfGroceris = 54.96;
+		
+		char middleInitial = 'M';
+		
+		boolean isHotOutside = false;
+		
+		String customerFirstName = 'Jon';
+		
+		String streetAddress = "1st Ave N";
+				
+		System.out.println(availablePlaneSeats);
+		System.out.println(costOfGroceris);
+		System.out.println(middleInitial);
+		System.out.println(isHotOutside);
+		System.out.println(customerFirstName);
+		System.out.println(streetAddress);
+		
+
+	}
+
+}
